c118e301dcdaa2acf7d4b2cd0ad9ccccdb6f8f30
Emad <emad.beyrami@gmail.com>
2025-10-12 23:18:35 +0200

    Added the example project

---
+++ Example/SnapShieldExample/SnapShieldExample/SnapShieldExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapShieldExampleApp.swift
+//  SnapShieldExample
+//
+//  Created by Emad on 2025-10-12.
+//
+
+import SwiftUI
+
+@main
+struct SnapShieldExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
